28af7f707c5a7a5958b242725dfd490b5d56e774
johannes.guenther <johannes.guenther.extern@idealo.de>
2020-03-20 23:08:05 +0100

    intial spring boot commit

---
+++ src/main/java/de/coronavirus/infektionsfall_uebermittellung/App.java
@@ -0,0 +1,13 @@
+package de.coronavirus.infektionsfall_uebermittellung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
---
+++ src/test/java/de/coronavirus/infektionsfall_uebermittellung/InfektionsfallUebermittellungApplicationTests.java
@@ -0,0 +1,13 @@
+package de.coronavirus.infektionsfall_uebermittellung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfektionsfallUebermittellungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

cd4df20c421d73829624e1fa616a0e1851850506
luxn <maximilian.herbers@outlook.com>
2020-03-21 13:01:57 +0100

    started working on PatientRepository and PatientService

--- server/src/main/java/de/coronavirus/imis/json/JsonController.java
+++ server/src/main/java/de/coronavirus/imis/json/JsonController.java
@@ -1,10 +1,8 @@
 package de.coronavirus.imis.json;
 
+import de.coronavirus.imis.domain.Patient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
@@ -15,16 +13,44 @@ public class JsonController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+
+    private PatientService patientService;
+
+    public JsonController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+
     @ResponseBody
     @GetMapping("/json")
     public InfectionSet sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
         return new InfectionSet(counter.incrementAndGet(), String.format(template, name));
     }
 
-    @ResponseBoy
+    @ResponseBody
     @GetMapping("/zip")
     public GetByZipVO returnCasesByZip() {
         // TODO: Get patients by zip and sum
         return new GetByZipVO(new HashMap<>());
+    }
+
+    @ResponseBody
+    @GetMapping("/json/patients")
+    public ResponseEntity<Iterable<Patient>> getPatients() {
+        return ResponseEntity.ok(patientService.getAllPatients());
+    }
+
+    @ResponseBody
+    @GetMapping("/json/patients/{id}")
+    public ResponseEntity<Patient> getPatients(@PathVariable("id") String id) {
+        try {
+            var patient = patientService.getPatientById(Long.valueOf(id));
+            if (patient.isPresent()) {
+                return ResponseEntity.ok(patient.get());
+            }
+        } catch (NumberFormatException e) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }

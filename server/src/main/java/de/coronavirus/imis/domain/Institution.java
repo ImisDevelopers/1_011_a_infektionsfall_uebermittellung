6af0593151fd8993162304d58479d494622cf2a3
Jonas Cirotzki <s8jociro@stud.uni-saarland.de>
2020-03-21 19:33:14 +0100

    Split address into fields

--- server/src/main/java/de/coronavirus/imis/domain/Institution.java
+++ server/src/main/java/de/coronavirus/imis/domain/Institution.java
@@ -9,7 +9,10 @@ import lombok.Data;
 @Data
 abstract class Institution {
     private String name;
-    private String address;
+    private String houseNumber;
+    private String street;
+    private String city;
+    private String zip;
     private String email;
     private String phoneNumber;
     private String comment;

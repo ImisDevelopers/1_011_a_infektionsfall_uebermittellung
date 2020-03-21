957569026907885dc80d9f1a35963eb0a5a362b6
Lars Gleim <gleim@cs.rwth-aachen.de>
2020-03-21 18:46:09 +0100

    Basic Layout

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <ExampleComponent/> -->
-    <TestingComponent/>
+    <a-layout id="components-layout-demo-responsive">
+      <a-layout-sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        @collapse="onCollapse"
+        @breakpoint="onBreakpoint"
+      >
+        <div class="logo" />
+        <a-menu theme="dark" mode="inline" :defaultSelectedKeys="['1']">
+          <a-menu-item key="1">
+            <a-icon type="user" />
+            <span class="nav-text">Verdachtsfälle</span>
+          </a-menu-item>
+          <a-menu-item key="2">
+            <a-icon type="video-camera" />
+            <span class="nav-text">Coronatests</span>
+          </a-menu-item>
+          <a-menu-item key="3">
+            <a-icon type="upload" />
+            <span class="nav-text">Befundupload</span>
+          </a-menu-item>
+        </a-menu>
+      </a-layout-sider>
+      <a-layout>
+        <a-layout-header :style="{ background: '#fff', padding: 0 }" />
+        <a-layout-content :style="{ margin: '0px auto' }">
+          <div :style="{ padding: '0px', background: '#fff', minHeight: '360px' }">
+            <TestingComponent/>
+          </div>
+        </a-layout-content>
+        <a-layout-footer style="textAlign: center">
+          IMIS ©2020 Created by WeVsVirus
+        </a-layout-footer>
+      </a-layout>
+    </a-layout>
   </div>
 </template>
 
@@ -22,7 +56,15 @@ export default {
     TestingComponent,
     // LabResultComponent,
     // ViewAllDataComponent,
-  }
+  },
+  methods: {
+    onCollapse(collapsed, type) {
+      console.log(collapsed, type);
+    },
+    onBreakpoint(broken) {
+      console.log(broken);
+    },
+  },
 }
 </script>
 
@@ -33,6 +75,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>

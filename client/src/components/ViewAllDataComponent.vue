9f65a3b729c6022d37ea5112a659a218abcc8bf3
Tobias Mahncke <tobias.mahncke@gmail.com>
2020-03-22 17:25:25 +0100

    Test data

--- client/src/components/ViewAllDataComponent.vue
+++ client/src/components/ViewAllDataComponent.vue
@@ -1,283 +1,309 @@
 <template>
-  <div>
-    <a-input-search placeholder="Suchen" style="width: 100%; margin: 10px 0"/>
-    <a-card class="table-container">
-      <a-table :columns="columns" :dataSource="data">
+    <div>
+        <a-input-search placeholder="Suchen" style="width: 100%; margin: 10px 0"/>
+        <a-card class="table-container">
+            <a-table :columns="columns" :dataSource="data">
         <span slot="riskAreas" slot-scope="riskAreas">
           <a-tag
-            v-for="riskArea in riskAreas"
-            :key="riskArea"
+                  v-for="riskArea in riskAreas"
+                  :key="riskArea"
           >
             {{riskArea}}
           </a-tag>
         </span>
-        <span slot="preIllnesses" slot-scope="preIllnesses">
+                <span slot="preIllnesses" slot-scope="preIllnesses">
           <a-tag
-            v-for="preIllness in preIllnesses"
-            :key="preIllness"
+                  v-for="preIllness in preIllnesses"
+                  :key="preIllness"
           >
             {{preIllness}}
           </a-tag>
         </span>
-      </a-table>
-      <div style="display: flex; width: 100%; justify-content: flex-end; margin-bottom: 1rem;">
-        <a-button type="primary" >CSV exportieren</a-button>
-      </div>
-    </a-card>
-  </div>
+            </a-table>
+            <div style="display: flex; width: 100%; justify-content: flex-end; margin-bottom: 1rem;">
+                <a-button type="primary">CSV exportieren</a-button>
+            </div>
+        </a-card>
+    </div>
 </template>
 
-// TestStationen Ordnen ProbeIds und durchzuführrende Tests den Patienten zu 
+// TestStationen Ordnen ProbeIds und durchzuführrende Tests den Patienten zu
 
 <script>
-const columns = [
-  {
-    title: 'ID',
-    dataIndex: 'id',
-    key: 'id'
-  },
-  {
-    title: 'Nachname',
-    dataIndex: 'lastName',
-    key: 'lastName'
-  },
-  {
-    title: 'Vorname',
-    dataIndex: 'firstName',
-    key: 'firstName'
-  },
-  {
-    title: 'Geschlecht',
-    dataIndex: 'gender',
-    key: 'gender'
-  },
-  {
-    title: 'Status',
-    dataIndex: 'status',
-    key: 'status'
-  },
-  {
-    title: 'Zeitpunkt',
-    dataIndex: 'timestamp',
-    key: 'timestamp'
-  },
-  {
-    title: 'Geburtsdatum',
-    dataIndex: 'birthDate',
-    key: 'birthDate'
-  },
-/*  {
-    title: 'Straße',
-    dataIndex: 'street',
-    key: 'street'
-  },
-  {
-    title: 'Hausnummer',
-    dataIndex: 'houseNumber',
-    key: 'houseNumber'
-  },
-  {
-    title: 'PLZ',
-    dataIndex: 'zip',
-    key: 'zip',
-    width: 150,
-  },*/
-  {
-    title: 'Stadt',
-    dataIndex: 'city',
-    key: 'city'
-  },
-  {
-    title: 'E-Mail',
-    dataIndex: 'email',
-    key: 'email'
-  },
-  {
-    title: 'Telefon',
-    dataIndex: 'phone',
-    key: 'phone'
-  },
-/*  {
-    title: 'Krankenkasse',
-    dataIndex: 'insuranceCompany',
-    key: 'insuranceCompany',
-    width: 150,
-  },
-  {
-    title: 'Versicherungsnummer',
-    dataIndex: 'insuranceMembershipNumber',
-    key: 'insuranceMembershipNumber',
-    width: 150,
-  },
-  {
-    title: 'Husten',
-    dataIndex: 'cough',
-    key: 'cough',
-    width: 150,
-  },
-  {
-    title: 'Abgeschlagenheit',
-    dataIndex: 'fatigue',
-    key: 'fatigue',
-    width: 150,
-  },
-  {
-    title: 'Fieber',
-    dataIndex: 'fever',
-    key: 'fever',
-    width: 150,
-  },
-  {
-    title: 'Luftnot',
-    dataIndex: 'shortnessOfBreath',
-    key: 'shortnessOfBreath',
-    width: 150,
-  },
-  {
-    title: 'Halskratzen',
-    dataIndex: 'soreThroat',
-    key: 'soreThroat',
-    width: 150,
-  },
-  {
-    title: 'Schnupfen',
-    dataIndex: 'cold',
-    key: 'cold',
-    width: 150,
-  },
-  {
-    title: 'Gliederschmerzen',
-    dataIndex: 'limbPain',
-    key: 'limbPain',
-    width: 150,
-  },
-  {
-    title: 'Kopfschmerzen',
-    dataIndex: 'headaches',
-    key: 'headaches',
-    width: 150,
-  },
-  {
-    title: 'Durchfall',
-    dataIndex: 'diarrhea',
-    key: 'diarrhea',
-    width: 150,
-  },
-  {
-    title: 'Beschwerden Auftrittsgeschw.',
-    dataIndex: 'symptoms',
-    key: 'symptoms',
-    width: 150,
-  },
-  {
-    title: 'Risikogebiet für Coronavirus',
-    dataIndex: 'riskAreas',
-    key: 'riskAreas',
-    scopedSlots: { customRender: 'riskAreas' },
-    width: 150,
-  },
-  {
-    title: 'Kontakt mit Corona-Patienten',
-    dataIndex: 'coronaContacts',
-    key: 'coronaContacts',
-    width: 150,
-  },
-  {
-    title: 'Vorerkrankungen',
-    dataIndex: 'preIllnesses',
-    key: 'preIllnesses',
-    scopedSlots: { customRender: 'preIllnesses' },
-    width: 150,
-  },
-  {
-    title: 'Geschwächtes Immunsystem',
-    dataIndex: 'weakenedImmuneSystem',
-    key: 'weakenedImmuneSystem',
-    width: 150,
-  },
-  {
-    title: 'Influenza-Impfung (2019/2020)',
-    dataIndex: 'fluImmunization',
-    key: 'fluImmunization',
-    width: 150,
-  },*/
-];
+    const columns = [
+        {
+            title: 'ID',
+            dataIndex: 'id',
+            key: 'id'
+        },
+        {
+            title: 'Nachname',
+            dataIndex: 'lastName',
+            key: 'lastName'
+        },
+        {
+            title: 'Vorname',
+            dataIndex: 'firstName',
+            key: 'firstName'
+        },
+        {
+            title: 'Geschlecht',
+            dataIndex: 'gender',
+            key: 'gender'
+        },
+        {
+            title: 'Status',
+            dataIndex: 'status',
+            key: 'status'
+        },
+        {
+            title: 'Zeitpunkt',
+            dataIndex: 'timestamp',
+            key: 'timestamp'
+        },
+        {
+            title: 'Geburtsdatum',
+            dataIndex: 'birthDate',
+            key: 'birthDate'
+        },
+        /*  {
+            title: 'Straße',
+            dataIndex: 'street',
+            key: 'street'
+          },
+          {
+            title: 'Hausnummer',
+            dataIndex: 'houseNumber',
+            key: 'houseNumber'
+          },
+          {
+            title: 'PLZ',
+            dataIndex: 'zip',
+            key: 'zip',
+            width: 150,
+          },*/
+        {
+            title: 'Stadt',
+            dataIndex: 'city',
+            key: 'city'
+        },
+        {
+            title: 'E-Mail',
+            dataIndex: 'email',
+            key: 'email'
+        },
+        {
+            title: 'Telefon',
+            dataIndex: 'phone',
+            key: 'phone'
+        },
+        /*  {
+            title: 'Krankenkasse',
+            dataIndex: 'insuranceCompany',
+            key: 'insuranceCompany',
+            width: 150,
+          },
+          {
+            title: 'Versicherungsnummer',
+            dataIndex: 'insuranceMembershipNumber',
+            key: 'insuranceMembershipNumber',
+            width: 150,
+          },
+          {
+            title: 'Husten',
+            dataIndex: 'cough',
+            key: 'cough',
+            width: 150,
+          },
+          {
+            title: 'Abgeschlagenheit',
+            dataIndex: 'fatigue',
+            key: 'fatigue',
+            width: 150,
+          },
+          {
+            title: 'Fieber',
+            dataIndex: 'fever',
+            key: 'fever',
+            width: 150,
+          },
+          {
+            title: 'Luftnot',
+            dataIndex: 'shortnessOfBreath',
+            key: 'shortnessOfBreath',
+            width: 150,
+          },
+          {
+            title: 'Halskratzen',
+            dataIndex: 'soreThroat',
+            key: 'soreThroat',
+            width: 150,
+          },
+          {
+            title: 'Schnupfen',
+            dataIndex: 'cold',
+            key: 'cold',
+            width: 150,
+          },
+          {
+            title: 'Gliederschmerzen',
+            dataIndex: 'limbPain',
+            key: 'limbPain',
+            width: 150,
+          },
+          {
+            title: 'Kopfschmerzen',
+            dataIndex: 'headaches',
+            key: 'headaches',
+            width: 150,
+          },
+          {
+            title: 'Durchfall',
+            dataIndex: 'diarrhea',
+            key: 'diarrhea',
+            width: 150,
+          },
+          {
+            title: 'Beschwerden Auftrittsgeschw.',
+            dataIndex: 'symptoms',
+            key: 'symptoms',
+            width: 150,
+          },
+          {
+            title: 'Risikogebiet für Coronavirus',
+            dataIndex: 'riskAreas',
+            key: 'riskAreas',
+            scopedSlots: { customRender: 'riskAreas' },
+            width: 150,
+          },
+          {
+            title: 'Kontakt mit Corona-Patienten',
+            dataIndex: 'coronaContacts',
+            key: 'coronaContacts',
+            width: 150,
+          },
+          {
+            title: 'Vorerkrankungen',
+            dataIndex: 'preIllnesses',
+            key: 'preIllnesses',
+            scopedSlots: { customRender: 'preIllnesses' },
+            width: 150,
+          },
+          {
+            title: 'Geschwächtes Immunsystem',
+            dataIndex: 'weakenedImmuneSystem',
+            key: 'weakenedImmuneSystem',
+            width: 150,
+          },
+          {
+            title: 'Influenza-Impfung (2019/2020)',
+            dataIndex: 'fluImmunization',
+            key: 'fluImmunization',
+            width: 150,
+          },*/
+    ];
 
-const data = [
-  {
-    id: "2837917348",
-    gender: "weiblich",
-    birthDate: "15.4.1982",
-    status: "Testergebnis ausstehend",
-    timestamp: "22.3.2020 11:20",
-    lastName: "Musterfrau",
-    firstName: "Marie",
-    city: "Musterstadt",
-    email: "marie.musterfrau@gmail.com",
-    phone: "0151 123456789"
-  }
-];
+    const data = [
+        {
+            id: "2837917348",
+            gender: "weiblich",
+            birthDate: "15.4.1982",
+            status: "Testergebnis ausstehend",
+            timestamp: "22.3.2020 11:20",
+            lastName: "Musterfrau",
+            firstName: "Marie",
+            city: "Musterstadt",
+            email: "marie.musterfrau@gmail.com",
+            phone: "0151 123456789"
+        },
+        {
+            id: "2837916548",
+            status: "Verdachtsfall",
+            timestamp: "20.3.2020 09:20",
+            birthDate: "05.11.1985",
+            "lastName": "Klein",
+            "firstName": "Kai",
+            "gender": "m\u00e4nnlich",
+            "dateOfBirth": "1927-4-5",
+            "email": "K@gmail.de",
+            "phoneNumber": "55510674222",
+            "street": "Nordstra\u00dfe",
+            "houseNumber": 43,
+            "zip": "6053824",
+            "city": "Bochum",
+            "insuranceCompany": "IKK Nord",
+            "insuranceMembershipNumber": "B676434522",
+            "fluImmunization": "Nein",
+            "speedOfSymptomsOutbreak": "Langsam",
+            "symptoms": ["Schnupfen", "Kopfschmerzen", "Fieber"],
+            "coronaContacts": "Ja",
+            "riskAreas": ["GrandEst"],
+            "weakenedImmuneSystem": false,
+            "preIllnesses": ["cancer"]
+        }
+    ];
 
-export default {
-  name: 'TestingComponent',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      entities: [],
-      content: "",
-      columns,
-      data
-    }
-  },
-  methods: {
-    create(e) {
-      e.preventDefault();
-      if (this.content.length > 0) {
-        fetch('/exampleEntities',
-                {
-                  method: 'POST',
-                  body: JSON.stringify({ content: this.content }),
-                  headers: {
-                    'Content-Type': 'application/json'
-                  }
+    export default {
+        name: 'TestingComponent',
+        props: {
+            msg: String
+        },
+        data() {
+            return {
+                entities: [],
+                content: "",
+                columns,
+                data
+            }
+        },
+        methods: {
+            create(e) {
+                e.preventDefault();
+                if (this.content.length > 0) {
+                    fetch('/exampleEntities',
+                        {
+                            method: 'POST',
+                            body: JSON.stringify({content: this.content}),
+                            headers: {
+                                'Content-Type': 'application/json'
+                            }
+                        })
+                        .then((response) => {
+                            return response.json();
+                        })
+                        .then((entity) => {
+                            this.entities.push(entity);
+                        });
+                    this.content = "";
+                }
+            }
+        },
+        created() {
+            fetch('/exampleEntities')
+                .then((response) => {
+                    return response.json();
                 })
-        .then((response) => {
-          return response.json();
-        })
-        .then((entity) => {
-          this.entities.push(entity);
-        });
-        this.content = "";
-      }
+                .then((entities) => {
+                    this.entities.push(...entities);
+                });
+        }
     }
-  },
-  created() {
-    fetch('/exampleEntities')
-    .then((response) => {
-      return response.json();
-    })
-    .then((entities) => {
-      this.entities.push(...entities);
-    });
-  }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 20px 10px;
-}
-span {
-  margin: 10px;
-}
+    h3 {
+        margin: 20px 10px;
+    }
 
-.table-container {
-  width: 100%;
-  /* overflow-x: scroll;
-  scroll-behavior: smooth; */
-}
+    span {
+        margin: 10px;
+    }
+
+    .table-container {
+        width: 100%;
+        /* overflow-x: scroll;
+        scroll-behavior: smooth; */
+    }
 
 </style>

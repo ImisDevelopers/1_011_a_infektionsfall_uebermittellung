fa859563b2c4ff1082f18dab9834e16b1e10b413
lukaskoeller <lukas-koeller@onlinehome.de>
2020-03-22 12:04:51 +0100

    Updated lab results component with tab view

--- client/src/components/LabResultComponent.vue
+++ client/src/components/LabResultComponent.vue
@@ -1,46 +1,61 @@
 <template>
-  <div style="width: 80%">
-    <a-form :form="form" :label-col="{ span: 7 }" :wrapper-col="{ span: 17 }" @submit="handleSubmit">
-      <a-card style="width: 500px">
-      <p style="text-align: left">Bitte melden Sie sich hier mit Laborkennung und Passwort an um einen Test als positiv / negativ zu kennzeichnen.</p>
-      <a-divider orientation="left" style="margin-top: 2rem;">Laboranmeldung</a-divider>
-        <a-form-item label="Laborkennung">
-          <a-input
-            v-decorator="['note', { rules: [{ required: true, message: 'Bitte geben Sie Ihre Laborkennung ein.' }] }]"
-            placeholder="z.B 1337-4237-9438"
-          />
-        </a-form-item>
-        <a-form-item label="Passwort">
-          <a-input
-            v-decorator="['note', { rules: [{ required: true, message: 'Bitte geben Sie Ihr Passwort ein.' }] }]"
-            type="password"
-          />
-        </a-form-item>
-        <a-divider orientation="left" style="margin-top: 2rem;">Testresultat eingeben</a-divider>
-        <a-form-item label="Test-ID">
-          <a-input
-            v-decorator="['note', { rules: [{ required: true, message: 'Bitte geben Sie Ihre Test-ID ein.' }] }]"
-            placeholder="z.B 1337-4237-9438"
-          />
-        </a-form-item>
-        <a-form-item label="Testresultat">
-          <a-radio-group v-decorator="['radio-group']">
-            <a-radio value="true">
-              Positiv
-            </a-radio>
-            <a-radio value="false">
-              Negativ
-            </a-radio>
-          </a-radio-group>
-        </a-form-item>
-        <a-divider />
-        <a-form-item :wrapper-col="{ span: 24, offset: 0 }">
-          <a-button type="primary" html-type="submit">
-            Abschicken
-          </a-button>
-        </a-form-item>
-      </a-card>
-    </a-form>
+  <div>
+    <a-tabs defaultActiveKey="1" v-model="activeKey" @change="callback">
+      <a-tab-pane tab="Laboranmeldung" key="1">
+        <a-card style="width: 500px; margin: 2rem auto; min-height: 300px">
+          <a-form :form="form" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+            <p>Bitte melden Sie sich hier mit Laborkennung und Passwort an um einen Test als positiv / negativ zu kennzeichnen.</p>
+            <!-- <a-divider orientation="left" style="margin-top: 2rem;">Laboranmeldung</a-divider> -->
+            <a-form-item label="Laborkennung">
+              <a-input
+                v-decorator="['note', { rules: [{ required: true, message: 'Bitte geben Sie Ihre Laborkennung ein.' }] }]"
+                placeholder="z.B 1337-4237-9438"
+              />
+            </a-form-item>
+            <a-form-item label="Passwort">
+              <a-input
+                v-decorator="['note', { rules: [{ required: true, message: 'Bitte geben Sie Ihr Passwort ein.' }] }]"
+                type="password"
+              />
+            </a-form-item>
+            <a-divider />
+            <a-form-item :wrapper-col="{ span: 24, offset: 0 }">
+              <a-button type="primary" html-type="submit" @click="handleLabSubmit">
+                Abschicken
+              </a-button>
+            </a-form-item>
+          </a-form>
+        </a-card>
+      </a-tab-pane>
+      <a-tab-pane tab="Testresultat eingeben" :disabled="isLoggedIn === false ? 'disabled' : false" key="2" forceRender>
+        <a-card style="width: 500px; margin: 2rem auto; min-height: 300px">
+          <a-form :form="form" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }" @submit="handleSubmit">
+            <a-form-item label="Proben-ID">
+              <a-input
+                v-decorator="['note', { rules: [{ required: true, message: 'Bitte geben Sie Ihre Test-ID ein.' }] }]"
+                placeholder="z.B 1337-4237-9438"
+              />
+            </a-form-item>
+            <a-form-item label="Testresultat">
+              <a-radio-group v-decorator="['radio-group']">
+                <a-radio value="true">
+                  Positiv
+                </a-radio>
+                <a-radio value="false">
+                  Negativ
+                </a-radio>
+              </a-radio-group>
+            </a-form-item>
+            <a-divider />
+            <a-form-item :wrapper-col="{ span: 24, offset: 0 }">
+              <a-button type="primary" html-type="submit">
+                Abschicken
+              </a-button>
+            </a-form-item>
+          </a-form>
+        </a-card>
+      </a-tab-pane>
+    </a-tabs>
   </div>
 </template>
 
@@ -55,7 +70,9 @@ export default {
   data() {
     return {
       entities: [],
-      content: ""
+      content: "",
+      activeKey: "1",
+      isLoggedIn: false
     }
   },
   methods: {
@@ -78,6 +95,10 @@ export default {
         });
         this.content = "";
       }
+    },
+    handleLabSubmit() {
+      this.isLoggedIn = true
+      this.activeKey = "2";
     }
   },
   created() {

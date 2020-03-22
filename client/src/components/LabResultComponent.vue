546bdfc57e6b1ccc2a892ac9085b67d4f0c0cad1
lukaskoeller <lukas-koeller@onlinehome.de>
2020-03-22 10:59:32 +0100

    Implemented lab-result form

--- client/src/components/LabResultComponent.vue
+++ client/src/components/LabResultComponent.vue
@@ -1,20 +1,46 @@
 <template>
-  <div>
-    <h3>Example Entities</h3>
-    <div v-if="entities.length === 0">
-      No Example Entities created yet...
-    </div>
-    <div v-else v-for="(entity, idx) in entities" :key="idx">
-      <span>{{entity.id}}</span>
-      <span>......</span>
-      <span>{{entity.content}}</span>
-    </div>
-    <h3>Create an Example Entity</h3>
-    <form @submit="create" autocomplete="off">
-      <span>Content:</span>
-      <input v-model="content"/>
-      <button>Create</button>
-    </form>
+  <div style="width: 80%">
+    <a-form :form="form" :label-col="{ span: 7 }" :wrapper-col="{ span: 17 }" @submit="handleSubmit">
+      <a-card style="width: 500px">
+      <p style="text-align: left">Bitte melden Sie sich hier mit Laborkennung und Passwort an um einen Test als positiv / negativ zu kennzeichnen.</p>
+      <a-divider orientation="left" style="margin-top: 2rem;">Laboranmeldung</a-divider>
+        <a-form-item label="Laborkennung">
+          <a-input
+            v-decorator="['note', { rules: [{ required: true, message: 'Bitte geben Sie Ihre Laborkennung ein.' }] }]"
+            placeholder="z.B 1337-4237-9438"
+          />
+        </a-form-item>
+        <a-form-item label="Passwort">
+          <a-input
+            v-decorator="['note', { rules: [{ required: true, message: 'Bitte geben Sie Ihr Passwort ein.' }] }]"
+            type="password"
+          />
+        </a-form-item>
+        <a-divider orientation="left" style="margin-top: 2rem;">Testresultat eingeben</a-divider>
+        <a-form-item label="Test-ID">
+          <a-input
+            v-decorator="['note', { rules: [{ required: true, message: 'Bitte geben Sie Ihre Test-ID ein.' }] }]"
+            placeholder="z.B 1337-4237-9438"
+          />
+        </a-form-item>
+        <a-form-item label="Testresultat">
+          <a-radio-group v-decorator="['radio-group']">
+            <a-radio value="true">
+              Positiv
+            </a-radio>
+            <a-radio value="false">
+              Negativ
+            </a-radio>
+          </a-radio-group>
+        </a-form-item>
+        <a-divider />
+        <a-form-item :wrapper-col="{ span: 24, offset: 0 }">
+          <a-button type="primary" html-type="submit">
+            Abschicken
+          </a-button>
+        </a-form-item>
+      </a-card>
+    </a-form>
   </div>
 </template>
 

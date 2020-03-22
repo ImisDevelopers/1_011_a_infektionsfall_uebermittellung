b77afdf71b736073c92863321cdabca43aed8eb0
DavidB <klausbart@gmx.net>
2020-03-22 14:21:20 +0100

    added example statistics

--- client/src/components/PublicStatisticsComponent.vue
+++ client/src/components/PublicStatisticsComponent.vue
@@ -19,6 +19,7 @@
             key: 'zip',
             width: 10,
             sorter: (a, b) => a.zip - b.zip,
+            defaultSortOrder: "ascend",
         },
         {
             title: 'Aktive Fälle (Infizierte)',
@@ -63,6 +64,41 @@
             suspicion: "342",
             recovered: "1435",
             deaths: "12",
+        },
+        {
+            zip: "17458",
+            active: "54",
+            suspicion: "68",
+            recovered: "125",
+            deaths: "14",
+        },
+        {
+            zip: "48569",
+            active: "256",
+            suspicion: "123",
+            recovered: "48",
+            deaths: "5",
+        },
+        {
+            zip: "45369",
+            active: "12",
+            suspicion: "25",
+            recovered: "2",
+            deaths: "0",
+        },
+        {
+            zip: "43985",
+            active: "36",
+            suspicion: "21",
+            recovered: "3",
+            deaths: "7",
+        },
+        {
+            zip: "68524",
+            active: "3425",
+            suspicion: "1258",
+            recovered: "125",
+            deaths: "35",
         }
     ];
     export default {

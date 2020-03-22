e794587b2cbe5935302475b279f53e80765758ab
Jonas Cirotzki <s8jociro@stud.uni-saarland.de>
2020-03-22 13:32:18 +0100

    Fix

--- server/src/main/java/de/coronavirus/imis/TestDataLoader.java
+++ server/src/main/java/de/coronavirus/imis/TestDataLoader.java
@@ -3,6 +3,7 @@ package de.coronavirus.imis;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.coronavirus.imis.api.dto.CreateInstitutionDTO;
 import de.coronavirus.imis.api.dto.CreatePatientDTO;
+import de.coronavirus.imis.services.InstitutionService;
 import de.coronavirus.imis.services.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -13,7 +14,6 @@ import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 
@@ -23,10 +23,12 @@ public class TestDataLoader implements ApplicationRunner {
 
 
     private final PatientService patientService;
+    private final InstitutionService institutionService;
 
     @Autowired
-    public TestDataLoader(PatientService patientService) {
+    public TestDataLoader(PatientService patientService, InstitutionService institutionService) {
         this.patientService = patientService;
+        this.institutionService = institutionService;
     }
 
     static Object makeDTO(String testFileName, Class clazz)
@@ -47,6 +49,9 @@ public class TestDataLoader implements ApplicationRunner {
         try {
             var patientDTO = (CreatePatientDTO) makeDTO("createPerson.json", CreatePatientDTO.class);
             patientService.addPatient(patientDTO);
+
+            var laboratoryDTO = (CreateInstitutionDTO) makeDTO("createInstitution.json", CreateInstitutionDTO.class);
+            institutionService.createLaboratoryInstitution(laboratoryDTO);
         } catch (IOException e) {
             e.printStackTrace();
         }

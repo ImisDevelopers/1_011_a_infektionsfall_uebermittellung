2ea4927a633a63cfdd112e09a664e59e08bc6529
johannes.guenther <johannes.guenther.extern@idealo.de>
2020-03-22 11:45:56 +0100

    stats generation

---
+++ server/src/main/java/de/coronavirus/imis/api/StatsControler.java
@@ -0,0 +1,27 @@
+package de.coronavirus.imis.api;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+import de.coronavirus.imis.api.dto.AggregationResultZip;
+import de.coronavirus.imis.services.StatsService;
+
+@RestController
+@RequestMapping("/stats")
+@RequiredArgsConstructor
+public class StatsControler {
+    private final StatsService statsService;
+
+    @GetMapping
+    public List<AggregationResultZip> getForZip(@RequestParam("lowerBoundsZip") String lowerBounds,
+            @RequestParam("upperBoundsZips") String upperBounds
+    ) {
+        return statsService.resultZipList(Integer.valueOf(lowerBounds), Integer.valueOf(upperBounds));
+    }
+}

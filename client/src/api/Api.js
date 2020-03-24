91c4edff63ac132de0d267e54eb3205f1007a9fe
Kuettner Tobias <tobias.kuettner@scandio.de>
2020-03-24 07:07:58 +0100

    Randomize personal date

--- client/src/api/Api.js
+++ client/src/api/Api.js
@@ -1,121 +1,120 @@
-class Api {  
-
-    constructor() {
-        if (location.host.includes("localhost") || location.host.includes("127.0.0.1")) {
-            this.BASE_URL = ""
-        } else [
-            this.BASE_URL = "https://api.imis-prototyp.de"
-        ]
+class Api {
+  constructor() {
+    if (
+      location.host.includes("localhost") ||
+      location.host.includes("127.0.0.1")
+    ) {
+      this.BASE_URL = "";
+    } else {
+      this.BASE_URL = "https://api.imis-prototyp.de"
     }
+  }
 
-    getCall(url) {
-        return fetch(encodeURI(`${this.BASE_URL}/${url}`), {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        }).then((response) => {
-            return response.json();
-        })
-    }
+  getCall(url) {
+    return fetch(encodeURI(`${this.BASE_URL}/${url}`), {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then(response => {
+      return response.json();
+    });
+  }
 
+  postCall(url, body) {
+    return fetch(encodeURI(`${this.BASE_URL}/${url}`), {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
+    }).then(response => {
+      return response.json();
+    });
+  }
 
-    postCall(url, body) {
-        return fetch(encodeURI(`${this.BASE_URL}/${url}`), {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(body)
-        }).then((response) => {
-            return response.json();
-        })
-    }
+  putCall(url, body) {
+    return fetch(encodeURI(`${this.BASE_URL}/${url}`), {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
+    }).then(response => {
+      return response.json();
+    });
+  }
 
-    putCall(url, body) {
-        return fetch(encodeURI(`${this.BASE_URL}/${url}`), {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(body)
-        }).then((response) => {
-            return response.json();
-        })
-    }
+  deleteCall(url) {
+    return fetch(encodeURI(`${this.BASE_URL}/${url}`), {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then(response => {
+      return response.json();
+    });
+  }
 
-    deleteCall(url) {
-        return fetch(encodeURI(`${this.BASE_URL}/${url}`), {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        }).then((response) => {
-            return response.json();
-        })
-    }
+  /*
+   * ----------------------------------
+   */
 
+  postDoctorCreateAppointment({ doctorId, laboratoryId, patientId }) {
+    return this.postCall("/doctor/create_appointment", {
+      doctorId,
+      laboratoryId,
+      patientId
+    });
+  }
 
-    /*
-     * ----------------------------------
-     */
+  getTestReports(testId) {
+    return this.getCall(`/test_report/${testId}`);
+  }
 
-    postDoctorCreateAppointment({doctorId, laboratoryId, patientId}) {
-        return this.postCall('/doctor/create_appointment', {
-            doctorId,
-            laboratoryId,
-            patientId
-        })
-    }
+  // TODO: postTestReport, putTestReport | how to handle multipart/form-data in fetch ?!
 
-    getTestReports(testId) {
-        return this.getCall(`/test_report/${testId}`)
-    }
+  getTestReport(testId) {
+    return this.getCall(`/test_report/${testId}`);
+  }
 
+  deleteTestReport(testId) {
+    return this.deleteCall(`/test_report/${testId}`);
+  }
 
-    // TODO: postTestReport, putTestReport | how to handle multipart/form-data in fetch ?!
+  postInstitution(institution) {
+    return this.postCall("/institutions", institution);
+  }
 
+  postLabtest(labtest) {
+    return this.postCall("/labtest", labtest);
+  }
 
-    getTestReport(testId) {
-        return this.getCall(`/test_report/${testId}`)
-    }
+  putLabtest({ updatedTestStatus }) {
+    return this.postCall("/labtest", updatedTestStatus);
+  }
 
-    deleteTestReport(testId) {
-        return this.deleteCall(`/test_report/${testId}`)
-    }
+  getLabtestByPatient(patientId) {
+    return this.getCall(`/labtest/patient/${patientId}`);
+  }
 
-    postInstitution(institution) {
-        return this.postCall('/institutions', institution)
-    }
+  getPatients() {
+    return this.getCall("/patients");
+  }
 
-    postLabtest(labtest) {
-        return this.postCall('/labtest', labtest)
-    } 
+  postPatient(patient) {
+    return this.postCall("/patients", patient);
+  }
 
-    putLabtest({updatedTestStatus}) {
-        return this.postCall('/labtest', updatedTestStatus)
-    } 
+  getPatient(id) {
+    return this.getCall(`/patients/${id}`);
+  }
 
-    getLabtestByPatient(patientId) {
-        return this.getCall(`/labtest/patient/${patientId}`)
-    } 
-    
-    getPatients() {
-        return this.getCall('/patients')
-    }
-
-    postPatient(patient) {
-        return this.postCall('/patients', patient)
-    }
-
-    getPatient(id) {
-        return this.getCall(`/patients/${id}`)
-    }
-
-    getStats(lowerBoundsZip, upperBoundsZip) {
-        return this.getCall(`/stats?lowerBoundsZip=${lowerBoundsZip}&upperBoundsZips=${upperBoundsZip}`)
-    }
-
+  getStats(lowerBoundsZip, upperBoundsZip) {
+    return this.getCall(
+      `/stats?lowerBoundsZip=${lowerBoundsZip}&upperBoundsZips=${upperBoundsZip}`
+    );
+  }
 }
 
-export default new Api()
+export default new Api();
--- server/src/main/java/de/coronavirus/imis/App.java
+++ server/src/main/java/de/coronavirus/imis/App.java
@@ -3,6 +3,9 @@ package de.coronavirus.imis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -13,4 +16,13 @@ public class App {
         SpringApplication.run(App.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*").allowedOrigins("https://imis-prototyp.de/");
+            }
+        };
+    }
 }

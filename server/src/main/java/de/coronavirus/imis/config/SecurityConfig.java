4b1b1d9c22e59ffc8dc0b48b5695fd2a3420b540
johannes.guenther <johannes.guenther.extern@idealo.de>
2020-03-26 22:47:39 +0100

    configuration for development

--- server/src/main/java/de/coronavirus/imis/config/SecurityConfig.java
+++ server/src/main/java/de/coronavirus/imis/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package de.coronavirus.imis.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,6 +20,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .http(Integer.parseInt(environment.getProperty("server.http.port", "8080"))) // http port defined in yml config file
                 .mapsTo(Integer.parseInt(environment.getProperty("server.port", "443"))); // https port defined in yml config file
 
-        http.requiresChannel().anyRequest().requiresSecure();
+        http.requiresChannel()
+                .antMatchers("/actuator/health")
+                .requiresInsecure()
+                .anyRequest().requiresSecure();
     }
 }

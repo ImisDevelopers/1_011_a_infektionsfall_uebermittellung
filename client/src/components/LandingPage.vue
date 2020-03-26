d19f06492e9caff0e7833c3464b0b390cfb6c86e
Tobias Mahncke <tobias.mahncke@gmail.com>
2020-03-26 21:05:53 +0100

    Updated landing page

--- client/src/components/LandingPage.vue
+++ client/src/components/LandingPage.vue
@@ -2,25 +2,46 @@
     <div class="main">
         <div class="left-part">
             <div>
-                <h1><img src="../assets/logo.png" aspect-ratio="1.7" style="vertical-align: top">Schnell. Einfach. Zuverlässig.</h1>
+                <h1><img src="../assets/logo.png" aspect-ratio="1.7" style="vertical-align: middle">
+                    <span style="margin-left: 15px">Schnell. Einfach. Zuverlässig.</span>
+                </h1>
                 <br>
-                <iframe allowfullscreen="true" id="youtube" v-bind:style="{width: '100%', height: 60/16*9+ 'vw'}" src="https://www.youtube.com/embed/XIIlMh3Lbsc">Video vom Browser nicht unterstützt.</iframe>
-            </div><br>
-            <p style="display:table-cell; width: 50%; padding-right: 5%">{{paragraph1}}</p>
-            <p style="display:table-cell; width: 50%; padding-right: 5%">{{paragraph2}}</p>
+                <table>
+                    <tr>
+                        <td width="60%">
+                            <iframe allowfullscreen="true" id="youtube"
+                                    v-bind:style="{width: '100%', height: 38/16*9+ 'vw'}"
+                                    src="https://www.youtube.com/embed/XIIlMh3Lbsc">Video vom Browser nicht unterstützt.
+                            </iframe>
+                        </td>
+                        <td width="40%">
+                            <div style="padding-left: 50px; text-align: justify">{{paragraph1}}</div>
+                            <br>
+                            <div style="padding-left: 50px; text-align: justify">{{paragraph2}}</div>
+                            <br>
+                            <a style="padding-left: 50px;" href="https://devpost.com/software/imis-infektions-melde-und-informations-system">Mehr im DevPost</a>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div>
+                <img src="../assets/vision.png" width="75%">
+            </div>
+            <br>
         </div>
         <div class="transition"></div>
         <div class="right-part">
             <h1>Hier geht's zur Live-Demo:</h1>
-            <a-button type="primary" shape="round" icon="arrow-right" size="large" href="/prototype" block>Zum Prototypen</a-button>
+            <a-button type="primary" shape="round" icon="arrow-right" size="large" href="/prototype" block>Zum
+                Prototypen
+            </a-button>
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        components: {
-        },
+        components: {},
         data() {
             return {
                 paragraph1: "Das Sammeln und Verwalten der aktuellen Infektionszahlen erfordert extrem viel Zeit und Ressourcen. IMIS ist unsere Lösung für eine einfache Infektionsfall-Erfassung für Ärzte, Gesundheitsämter und Labore. Damit ermöglichen wir, dass wichtige Ressourcen an anderer Stelle besser eingesetzt werden können.",
@@ -41,8 +62,8 @@
     }
 
     .left-part {
-        display:table-cell;
-        width: 60vw;
+        display: table-cell;
+        width: 70vw;
         height: 100vh;
         vertical-align: center;
         text-align: left;
@@ -51,17 +72,18 @@
 
     .transition {
         display: table-cell;
-        background-image: linear-gradient(80deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0) 50%, lightgrey 50%, lightgrey 100%);
-        width: 15vw;
+        background-image: linear-gradient(80deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0) 50%, lightgrey 50%, lightgrey 100%);
+        width: 10vw;
         height: 100vh;
     }
 
     .right-part {
         display: table-cell;
-        width: 25vw;
+        width: 20vw;
         height: 100vh;
         background-color: lightgrey;
         text-align: left;
         vertical-align: middle;
+        padding-right: 40px;
     }
 </style>

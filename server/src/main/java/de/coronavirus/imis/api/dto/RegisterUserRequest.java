cdf6b43280c6f43b418ed7e3fcddd926e94c6b56
luxn <maximilian.herbers@outlook.com>
2020-03-27 22:04:27 +0100

    fix: Lombok Annotations

--- server/src/main/java/de/coronavirus/imis/api/dto/RegisterUserRequest.java
+++ server/src/main/java/de/coronavirus/imis/api/dto/RegisterUserRequest.java
@@ -2,8 +2,10 @@ package de.coronavirus.imis.api.dto;
 
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class RegisterUserRequest extends InstitutionDTO {
     private String userName;
     private String password;

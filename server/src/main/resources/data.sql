126b97d7d1e46c74db5c99fb26315a6f5efd3662
Henning Wobken <henning.wobken@simplex24.de>
2020-03-29 21:13:14 +0200

    #114 updated test data sql

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,12 +1,17 @@
+/* We do not want to conflict with hibernates sequence for primary keys */
 INSERT into users (id, password, username)
-values (1, '$2y$10$50NQ8FBG.soMzekSp8JF9ODOTYxysxyJxzovUNtXkCbAjoJxl0nq2', 'test_lab');
+values (1000001, '$2y$10$50NQ8FBG.soMzekSp8JF9ODOTYxysxyJxzovUNtXkCbAjoJxl0nq2', 'test_lab');
 INSERT into users (id, password, username)
-values (2, '$2y$10$50NQ8FBG.soMzekSp8JF9ODOTYxysxyJxzovUNtXkCbAjoJxl0nq2', 'test_doctor');
+values (1000002, '$2a$10$Dctp4SB7zWJZV7yASfTp.umRJ9GJVDABj6hWfOpDM9ho9XiIb2ktq', 'test_doctor');
 INSERT into authority(id, display_name, role)
-values (1, 'labor', 'LABORATORY');
+values (1, 'Labor', 'LABORATORY');
 INSERT into authority(id, display_name, role)
 VALUES (2, 'Doktor', 'DOCTORS_OFFICE');
+INSERT into authority(id, display_name, role)
+VALUES (3, 'Klinik', 'CLINIC');
+INSERT into authority(id, display_name, role)
+VALUES (4, 'Teststelle', 'TEST_SITE');
 INSERT into users_roles (user_id, roles_id)
-VALUES (1, 1);
+VALUES (1000001, 1);
 INSERT into users_roles (user_id, roles_id)
-VALUES (2, 2);
+VALUES (1000002, 2);

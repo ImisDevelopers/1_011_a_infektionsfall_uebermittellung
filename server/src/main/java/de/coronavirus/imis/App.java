1ef225e95a09bc2e207ec496eb39da06086685e2
Henning Wobken <henning.wobken@simplex24.de>
2020-03-29 13:34:40 +0200

    #108 more fun with cors

--- server/src/main/java/de/coronavirus/imis/App.java
+++ server/src/main/java/de/coronavirus/imis/App.java
@@ -2,9 +2,6 @@ package de.coronavirus.imis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -15,16 +12,4 @@ public class App {
         SpringApplication.run(App.class, args);
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://imis-prototyp.de", "https://staging.imis-prototyp.de",
-                                "http://imis-prototyp.de", "http://localhost:8080")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
-            }
-        };
-    }
 }

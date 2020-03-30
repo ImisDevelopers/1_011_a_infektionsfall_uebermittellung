381231fde4d61975ebe64bd893cb6faf5d9a05a2
Michael Sprauer <Michael.Sprauer@sap.com>
2020-03-31 01:01:38 +0200

    enable grafana & prometheus

--- chart/cluster-setup.sh
+++ chart/cluster-setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-helm upgrade --install sentry stable/sentry \
+# Will take ages for the first run...
+helm upgrade --install --atomic sentry stable/sentry \
   --set ingress.enabled=true,ingress.hostname=sentry.imis-prototyp.de,service.type=ClusterIP \
   --set ingress.annotations."kubernetes.io\/ingress.class"=traefik
+
+helm upgrade --install --atomic prometheus stable/prometheus \
+  --set server.global.scrape_interval=30s
+
+helm upgrade --install --atomic grafana stable/grafana \
+  --set "ingress.enabled=true,ingress.hosts={grafana.imis-prototyp.de}" \
+  --set "persistence.enabled=true"

3e3c5c91638334671dda73b946fd42511d4c19c2
johannes.guenther <johannes.guenther.extern@idealo.de>
2020-04-02 23:16:14 +0200

    minor cleanup

--- server/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-/* We do not want to conflict with hibernates sequence for primary keys */
-INSERT into users (id, password, username)
-values (1000002, '$2a$10$Dctp4SB7zWJZV7yASfTp.umRJ9GJVDABj6hWfOpDM9ho9XiIb2ktq', 'test_doctor');

a6324c3573c1a1b74405db5b26070bdeac8564a6
Jonathan <git@jonathanmengedoht.com>
2020-04-06 14:11:18 +0200

    change username for demo access

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 /* We do not want to conflict with hibernates sequence for primary keys */
 INSERT into users (id, password, username)
-values (1000001, '$2y$10$50NQ8FBG.soMzekSp8JF9ODOTYxysxyJxzovUNtXkCbAjoJxl0nq2', 'test_lab');
+values (1000001, '$2y$10$50NQ8FBG.soMzekSp8JF9ODOTYxysxyJxzovUNtXkCbAjoJxl0nq2', 'test');
 INSERT into users (id, password, username)
 values (1000002, '$2a$10$Dctp4SB7zWJZV7yASfTp.umRJ9GJVDABj6hWfOpDM9ho9XiIb2ktq', 'test_doctor');
 INSERT into authority(id, display_name, role)

408672897684593adad129f33f4dc5f793b57b99
DavidB <klausbart@gmx.net>
2020-04-07 16:52:46 +0200

    Getting a patients details from overview now requires doublecÃlick (UX)

--- client/src/views/PatientList.vue
+++ client/src/views/PatientList.vue
@@ -337,7 +337,7 @@ export default Vue.extend({
     customRow(record: Patient) {
       return {
         on: {
-          click: () => this.handlePatientClick(record),
+          dblclick: () => this.handlePatientClick(record),
         },
       }
     },

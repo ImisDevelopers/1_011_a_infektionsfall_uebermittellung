f5d2fb80aefdc05ec99cac2210a11186116abb2a
johannes.guenther <johannes.guenther.extern@idealo.de>
2020-04-09 21:14:03 +0200

    swagger generation

---
+++ server/src/test/java/de/coronavirus/imis/GenerateSwagger.java
@@ -0,0 +1,33 @@
+package de.coronavirus.imis;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class GenerateSwagger {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    void generateSwagger() throws Exception {
+        var result = mvc.perform(MockMvcRequestBuilders.get("/v2/api-docs"))
+                .andExpect(content()
+                        .contentType(MediaType.APPLICATION_JSON)).andReturn();
+        System.out.print(result.getResponse().getContentAsString());
+        Files.write(Paths.get("build/resources/swagger.json"), List.of(result.getResponse().getContentAsString()));
+    }
+
+}

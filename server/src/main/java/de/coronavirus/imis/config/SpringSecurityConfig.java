855d39025d8401d0ece538fd51361a1a60307a82
johannes.guenther <johannes.guenther.extern@idealo.de>
2020-04-25 17:17:40 +0200

    optimized security configuration and added test to validate security functions

--- server/src/main/java/de/coronavirus/imis/config/SpringSecurityConfig.java
+++ server/src/main/java/de/coronavirus/imis/config/SpringSecurityConfig.java
@@ -1,8 +1,9 @@
 package de.coronavirus.imis.config;
 
-import de.coronavirus.imis.config.domain.UserRole;
-import de.coronavirus.imis.domain.InstitutionType;
-import lombok.RequiredArgsConstructor;
+import static org.springframework.http.HttpMethod.OPTIONS;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.PUT;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,7 +16,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.http.HttpMethod.*;
+import lombok.RequiredArgsConstructor;
+
+import de.coronavirus.imis.config.domain.UserRole;
+import de.coronavirus.imis.domain.InstitutionType;
 
 @Configuration
 @EnableWebSecurity
@@ -30,6 +34,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	private final String GOVERNMENT_AGENCY = InstitutionType.GOVERNMENT_AGENCY.name();
 	private final String LABORATORY = InstitutionType.LABORATORY.name();
 	private final String TEST_SITE = InstitutionType.TEST_SITE.name();
+	private static final String API_PREFIX = "/api";
 
 
 	@Bean
@@ -47,32 +52,33 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		// FIXME route permissions
 		//@formatter:off
-        http
-                .httpBasic().disable()
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
-                // OPTIONS matcher for CORS, do not delete
-                .antMatchers(OPTIONS, "/**").permitAll()
-                .antMatchers(POST, "/patients").permitAll()
-                .antMatchers("/patients").hasAnyRole(DEPARTMENT_OF_HEALTH, CLINIC, DOCTORS_OFFICE, TEST_SITE)
-                .antMatchers("/doctor/*").hasAnyRole(DEPARTMENT_OF_HEALTH, DOCTORS_OFFICE, CLINIC)
-                .mvcMatchers(POST, "/labtest").hasAnyRole(DEPARTMENT_OF_HEALTH, DOCTORS_OFFICE, CLINIC, TEST_SITE)
-                .antMatchers("/labtest/patient/*").hasAnyRole(DEPARTMENT_OF_HEALTH, DOCTORS_OFFICE, CLINIC, TEST_SITE)
-                .mvcMatchers(PUT, "/labtest/*").hasAnyRole(DEPARTMENT_OF_HEALTH, LABORATORY)
-                .antMatchers("/stats").authenticated()
-                .antMatchers("/auth").permitAll()
-                .antMatchers("/auth/register").hasAuthority(UserRole.USER_ROLE_ADMIN.name())
-                .antMatchers("/institutions/*").hasAuthority(UserRole.USER_ROLE_ADMIN.name())
-                .antMatchers(
-                        "/v2/api-docs",
-                        "/swagger-resources/**",
-                        "/swagger-ui.html",
-                        "/webjars/**" ,
-                        /*Probably not needed*/ "/swagger.json").permitAll()
-                .antMatchers("/**").permitAll();
-        //@formatter:on
+		http
+				.httpBasic().disable()
+				.csrf().disable()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and()
+				.authorizeRequests()
+				// OPTIONS matcher for CORS, do not delete
+				.antMatchers(OPTIONS, "/**").permitAll()
+				.antMatchers("/api/auth").permitAll()
+				.antMatchers(API_PREFIX + "/patients").hasAnyRole(DEPARTMENT_OF_HEALTH, CLINIC, DOCTORS_OFFICE, TEST_SITE)
+				.antMatchers(API_PREFIX + "/doctor/*").hasAnyRole(DEPARTMENT_OF_HEALTH, DOCTORS_OFFICE, CLINIC)
+				.mvcMatchers(POST, API_PREFIX + "/labtest").hasAnyRole(DEPARTMENT_OF_HEALTH, DOCTORS_OFFICE, CLINIC, TEST_SITE)
+				.antMatchers(API_PREFIX + "/labtest/patient/*").hasAnyRole(DEPARTMENT_OF_HEALTH, DOCTORS_OFFICE, CLINIC, TEST_SITE)
+				.mvcMatchers(PUT, API_PREFIX + "/labtest/*").hasAnyRole(DEPARTMENT_OF_HEALTH, LABORATORY)
+				.antMatchers(API_PREFIX + "/stats").authenticated()
+				.antMatchers(API_PREFIX + "/auth/register").hasAuthority(UserRole.USER_ROLE_ADMIN.name())
+				.antMatchers(API_PREFIX + "/institutions/*").hasAuthority(UserRole.USER_ROLE_ADMIN.name())
+				.antMatchers(
+						"/v2/api-docs",
+						"/swagger-resources/**",
+						"/swagger-ui.html",
+						"/webjars/**",
+						/*Probably not needed*/ "/swagger.json").permitAll()
+				.antMatchers(API_PREFIX + "/auth/**").fullyAuthenticated()
+				.antMatchers(API_PREFIX + "/**").fullyAuthenticated()
+				.antMatchers("/**").permitAll();
+		//@formatter:on
 		http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
 	}
 

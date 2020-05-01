241b19869137d33f97cbf9886a9643ba7cc63cd5
Henning Wobken <henning.wobken@simplex24.de>
2020-05-01 22:40:46 +0200

    #251 remove searchbar

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -10,17 +10,7 @@
       />
       <span class="imis-title" style="color: white">IMIS</span>
     </div>
-    <a-form :form="form" style="flex: 0 1 700px; display: flex; align-items: center">
-      <a-button @click="searchPatient" class="search-link" html-type="submit" type="link">
-        <a-icon type="search" />
-      </a-button>
-      <PatientInput
-        :form="form"
-        :validation="['patientQuery']"
-        @select="selectPatient"
-        style="margin: 0; flex: 1 1 auto"
-      />
-    </a-form>
+    <span style="flex: 1 1 auto"></span>
     <div style="flex: 0 0 auto">
       <a-icon
         style="color: #aaa; padding: 1.5rem"
@@ -32,7 +22,6 @@
 </template>
 <script lang="ts">
 import Vue from 'vue'
-import PatientInput from '@/components/PatientInput.vue'
 import { authMapper } from '@/store/modules/auth.module'
 
 export default Vue.extend({
@@ -45,23 +34,10 @@ export default Vue.extend({
       return this.user?.username || ''
     },
   },
-  components: {
-    PatientInput,
-  },
   data() {
     return {
       form: this.$form.createForm(this),
     }
-  },
-  methods: {
-    searchPatient() {
-      this.$router.push({ name: 'patient-list', query: { query: this.form.getFieldValue('patientQuery') } }).catch(err => {
-        console.warn('Received ' + err.name + ' while navigating to patient list')
-      })
-    },
-    selectPatient(patientId: string) {
-      this.$router.push({ name: 'patient-detail', params: { id: patientId } })
-    },
   },
 })
 </script>

9008847954ed9c1880d43bb3cb3bd6d28f3264a4
Jochen-Sch <jochen.schoenenberg@web.de>
2020-05-11 17:16:37 +0200

    Sync Test Capture from Dev
    Screen for 'Zuordnung Test'
    test-types + test-materials sync from dev

--- client/src/models/test-types.ts
+++ client/src/models/test-types.ts
@@ -3,17 +3,14 @@ export type TestType = 'PCR' | 'ANTIBODY';
 export interface TestTypeItem {
   id: TestType;
   label: string;
-  icon: string;
 }
 
 export const testTypes: TestTypeItem[] = [
   {
     id: 'ANTIBODY',
     label: 'Antikörper',
-    icon: 'search',
   }, {
     id: 'PCR',
     label: 'PCR',
-    icon: 'link',
   },
 ]

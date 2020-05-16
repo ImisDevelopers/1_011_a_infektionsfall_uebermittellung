6bed4d4479f05248f9ddf2a34c7c882e66d68a90
Jan-Niklas Brandes <janniklasbrandes@aol.de>
2020-05-16 22:12:31 +0200

    Hide search bar from statistics view

--- client/src/views/PublicStatistics.vue
+++ client/src/views/PublicStatistics.vue
@@ -3,7 +3,7 @@
     <a-affix style="text-align: center; font-weight: bold; font-size: 18pt; padding: 10px;">
       Statische Testdaten
     </a-affix>
-    <a-input-search placeholder="Suchen" style="width: 100%; margin: 10px 0" />
+    <!--<a-input-search placeholder="Suchen" style="width: 100%; margin: 10px 0" />-->
     <a-card class="table-container">
       <a-table :columns="columns" :dataSource="data" :scroll="{x: 1, y: 0}">
       </a-table>

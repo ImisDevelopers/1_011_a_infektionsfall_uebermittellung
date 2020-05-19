bd2d502d1a7ac4aab992e76138305f85e39c29ee
Tobias Bodor <tobias.bodor@gmail.com>
2020-05-19 13:44:05 +0200

    #319 Resolve merge conflicts with master

--- server/src/main/java/de/coronavirus/imis/api/ExposureContactController.java
+++ server/src/main/java/de/coronavirus/imis/api/ExposureContactController.java
@@ -1,39 +1,19 @@
 package de.coronavirus.imis.api;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 import de.coronavirus.imis.api.converter.ExOrNewContactConverter;
-import lombok.RequiredArgsConstructor;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
 import de.coronavirus.imis.api.dto.ExposureContactDTO;
 import de.coronavirus.imis.mapper.ExposureContactMapper;
 import de.coronavirus.imis.repositories.ExposureContactRepository;
 import de.coronavirus.imis.services.util.BulkOperationService;
-import static de.coronavirus.imis.services.util.BulkOperationService.*;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static de.coronavirus.imis.services.util.BulkOperationService.BulkRequest;
+import static de.coronavirus.imis.services.util.BulkOperationService.ItemStatus;
 
 
 @RestController
@@ -48,7 +28,8 @@ public class ExposureContactController {
 
   @PostMapping
   public ExposureContactDTO.FromServer createExposureContact(@RequestBody ExposureContactDTO.ToServer dto) {
-    dto.setContact(exOrNewContactConverter.convert(dto.getContact()));return exposureContactMapper.toExposureContactDTO(
+    dto.setContact(exOrNewContactConverter.convert(dto.getContact()));
+    return exposureContactMapper.toExposureContactDTO(
       exposureContactRepository.saveAndFlush(
         exposureContactMapper.toExposureContact(dto)
       )
@@ -72,13 +53,14 @@ public class ExposureContactController {
   @GetMapping("/by-source/{id}")
   public List<ExposureContactDTO.FromServer> getExposureContactsForPatient(@PathVariable("id") String patientId) {
     return exposureContactRepository.findBySourceId(patientId)
-      .stream().map(contact -> exposureContactMapper.toExposureContactDTO(contact))
+      .stream().map(exposureContactMapper::toExposureContactDTO)
       .collect(Collectors.toList());
   }
+
   @GetMapping("/by-contact/{id}")
   public List<ExposureContactDTO.FromServer> getExposureSourceContactsForPatient(@PathVariable("id") String patientId) {
     return exposureContactRepository.findByContactId(patientId)
-      .stream().map(contact -> exposureContactMapper.toExposureContactDTO(contact))
+      .stream().map(exposureContactMapper::toExposureContactDTO)
       .collect(Collectors.toList());
   }
 
@@ -120,6 +102,7 @@ public class ExposureContactController {
   public static class BulkInsertOptions {
     private boolean allowOverride;
   }
+
   public enum OpResult {
     CREATE,
     OVERRIDE,

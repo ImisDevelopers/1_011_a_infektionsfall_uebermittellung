e454eb549abd5d73b5f2806a55a529e137b20036
Leonard Koll <leko@netlight.com>
2020-05-22 12:06:06 +0200

    incidents passed

--- client/src/components/Incidents.vue
+++ client/src/components/Incidents.vue
@@ -1,11 +1,15 @@
 <template>
-    <p>Coming Soon :)</p>
+    <p>{{incidents[0].id}}</p>
 </template>
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
   name: 'Incidents',
+  created() {
+      console.log(this.incidents)
+  },
+  props: ['incidents'],
   data() {
     return {}
   },

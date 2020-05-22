b68f1e2528c5b5e925222ee95df15cf4aae491e4
syntakker <>
2020-05-22 19:34:29 +0000

    UI Quick Fixes #379: Änderungen Dropdown Menü Berufe auf Seite 4
        1. "Pflegepersonal" umbenannt in "Krankenpfleger-in"
        2. Letzten Punkt der Liste "Keiner der genannten (bitte unten eingeben)" geändert in "Anderer"
        3. angeglichen: Altenpflege -> Altenpfleger-in
        4. Alphabetische Einträge (bis auf "Anderer")

--- client/src/models/risk-occupation.ts
+++ client/src/models/risk-occupation.ts
@@ -7,17 +7,17 @@ export interface RiskOccupationOption {
 
 export const RISK_OCCUPATIONS: RiskOccupationOption[] = [
   { value: 'DOCTOR', label: 'Arzt/Ärztin' },
-  { value: 'NURSE', label: 'Pflegepersonal' },
-  { value: 'CAREGIVER', label: 'Altenpflege' },
+  { value: 'CAREGIVER', label: 'Altenpfleger-in' },
   {
     value: 'FIRE_FIGHTER_POLICE',
     label: 'Gefahrenabwehr (Polizei, Feuerwehr usw.)',
   },
+  { value: 'NURSE', label: 'Krankenpfleger-in' },
+  { value: 'TEACHER', label: 'Lehrer-in/Kindergärtner-in' },
   { value: 'PUBLIC_ADMINISTRATION', label: 'Öffentliche Verwaltung' },
   { value: 'STUDENT', label: 'Schüler-in' },
-  { value: 'TEACHER', label: 'Lehrer-in/Kindergärtner-in' },
   {
     value: 'NO_RISK_OCCUPATION',
-    label: 'Keiner der genannten (bitte unten eingeben)',
+    label: 'Anderer',
   },
 ]

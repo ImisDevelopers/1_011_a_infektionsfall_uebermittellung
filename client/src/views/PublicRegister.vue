8b4b6fde286de3e41b713dbae9c87188d6c6be11
syntakker <>
2020-05-23 16:58:22 +0000

    UI Quick Fixes #379: Änderungen am Button Daten übermitteln
    1. Icon gegen Papierflieger ausgetauscht
    2. Text "Abschließen" ergänzt zu "Abschließen und Daten übermitteln"

--- client/src/views/PublicRegister.vue
+++ client/src/views/PublicRegister.vue
@@ -164,8 +164,17 @@
                   style="margin-right: 7px;"
                   type="check-circle"
                 />
-                Abschließen
+                Abschließen und Daten übermitteln
               </h2>
+              <div
+                style="
+                  margin-bottom: 34px;
+                  display: flex;
+                  align-items: center;
+                  justify-content: center;
+                "
+              >
+              </div>
               <a-form-item>
                 <a-checkbox @change="onCheckedChange">
                   Ich erkläre mich mit der Übermittlung meiner Daten zur
@@ -241,7 +250,14 @@
           style="flex: 0 1 150px;"
           type="primary"
         >
-          <a-icon type="save" />
+          <a-icon>
+            <svg viewBox="64 64 896 896" focusable="false" class="" data-icon="send" width="1em" height="1em"
+	          fill="currentColor" aria-hidden="true">
+	          <path
+		        d="M931.4 498.9L94.9 79.5c-3.4-1.7-7.3-2.1-11-1.2a15.99 15.99 0 00-11.7 19.3l86.2 352.2c1.3 5.3 5.2 9.6 10.4 11.3l147.7 50.7-147.6 50.7c-5.2 1.8-9.1 6-10.3 11.3L72.2 926.5c-.9 3.7-.5 7.6 1.2 10.9 3.9 7.9 13.5 11.1 21.5 7.2l836.5-417c3.1-1.5 5.6-4.1 7.2-7.1 3.9-8 .7-17.6-7.2-21.6zM170.8 826.3l50.3-205.6 295.2-101.3c2.3-.8 4.2-2.6 5-5 1.4-4.2-.8-8.7-5-10.2L221.1 403 171 198.2l628 314.9-628.2 313.2z"
+	          />
+            </svg>
+          </a-icon>
           Absenden
         </a-button>
       </div>

d7677554e60f34d73edcb18557b13cec3ac14d69
Leonard Koll <leko@netlight.com>
2020-05-26 15:44:14 +0200

    sort of works

--- server/src/main/java/de/coronavirus/imis/config/HibernateSearchConfig.java
+++ server/src/main/java/de/coronavirus/imis/config/HibernateSearchConfig.java
@@ -5,16 +5,17 @@ import javax.persistence.EntityManagerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import de.coronavirus.imis.services.SearchService;
+//import de.coronavirus.imis.services.SearchService;
 
 @Configuration
 public class HibernateSearchConfig {
 
+	/*
 	@Bean
 	public SearchService searchService(EntityManagerFactory emf) {
 		var searchService = new SearchService(emf);
 		searchService.triggerIndexing();
 		return searchService;
 	}
-
+*/
 }

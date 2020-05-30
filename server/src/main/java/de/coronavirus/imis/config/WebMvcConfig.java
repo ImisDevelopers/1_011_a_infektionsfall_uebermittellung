264c0f2e049cdd5cb8dbae0d3dfcc5c854829918
Michael Sprauer <Michael.Sprauer@sap.com>
2020-05-30 23:35:50 +0200

    remove cors-mapping since we are "same origin"

--- server/src/main/java/de/coronavirus/imis/config/WebMvcConfig.java
+++ server/src/main/java/de/coronavirus/imis/config/WebMvcConfig.java
@@ -7,18 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-				.allowedOrigins("https://imis-prototyp.de",
-						"https://staging.imis-prototyp.de",
-						"https://pilot.imis-prototyp.de",
-						"http://imis-prototyp.de",
-						"http://localhost:8080",
-						"https://imis-pr-*.a.run.app")
-				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-				.allowCredentials(true);
-	}
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
